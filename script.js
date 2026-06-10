a290800224ff9b4f2e282de4a10bcc5d8318375d
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-10 14:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+//Write your code here 
+  const result = [];
+  let subArr = [];
+  let currentSum = 0; 
+
+  for (let i = 0; i < arr.length; i++) {
+    if (currentSum + arr[i] <= n) {
+      subArr.push(arr[i]);
+      currentSum += arr[i];
+    } else {
+      result.push(subArr);
+      subArr = [arr[i]];
+      currentSum = arr[i];
+    }
+  }
+
+  if (subArr.length > 0) {
+    result.push(subArr);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
